f8b58bbdefff81dc9e9d4a5b662908554057f1a8
1sunJ <won9619v@naver.com>
2024-10-10 02:19:04 +0900

    스프링 부트 프로젝트 생성

---
+++ src/main/java/com/ariari/ariari/AriariApplication.java
@@ -0,0 +1,13 @@
+package com.ariari.ariari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriariApplication.class, args);
+	}
+
+}
