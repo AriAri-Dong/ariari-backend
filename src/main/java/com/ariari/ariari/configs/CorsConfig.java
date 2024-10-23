3a58cd7b7ebc3a5e0b7390adc8d1235fcf8530c9
대선 <iuholic83@naver.com>
2024-10-23 17:17:55 +0900

    Feat : Cors 처리 추가 완료

---
+++ src/main/java/com/ariari/ariari/configs/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ariari.ariari.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // 모든 출처 허용 , 플러터로 네이티브 앱을 만들 시에 따로 경로가 존재하지 않기 때문에 모든 경로를 허용해야한다.
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");  // 모든 헤더 허용
+            }
+        };
+    }
+}
