1c0361e71088620167912221f8a1ba6df64b49be
1sunJ <won9619v@naver.com>
2024-11-02 01:47:29 +0900

    Feat : ViewsManager 추가

--- src/main/java/com/ariari/ariari/commons/manager/ViewsManager.java
+++ src/main/java/com/ariari/ariari/commons/manager/ViewsManager.java
@@ -6,6 +6,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,7 +16,7 @@ import java.util.concurrent.TimeUnit;
  * 2. 중복 조회인지 확인
  *  -> find in redis (CLUB + clubId + "_" + clientId)
  * 3. 조회 수 처리
- *  -> CLUB31_1101 : 1 (views) (expired : 14 days)
+ *  -> CLUB31_11-01 : 1 (views) (expired : 14 days)
  */
 @Component
 @RequiredArgsConstructor
@@ -26,21 +28,37 @@ public class ViewsManager {
     private static final int VIEW_EXPIRED_DAYS = 14;
 
     public void addClientIp(ViewsContentType viewsContentType, Long id, String clientIp) {
-        String key = resolveKey(viewsContentType, id, clientIp);
+        String key = resolveKeyForDuplicateView(viewsContentType, id, clientIp);
         redisTemplate.opsForValue().set(key, 1, VIEW_DUPLICATE_EXPIRED_DAYS, TimeUnit.DAYS);
     }
 
     public boolean checkForDuplicateView(ViewsContentType viewsContentType, Long id, String clientIp) {
-        String key = resolveKey(viewsContentType, id, clientIp);
+        String key = resolveKeyForDuplicateView(viewsContentType, id, clientIp);
         Integer result = redisTemplate.opsForValue().get(key);
         return result != null;
     }
 
-    public void addViews() {
+    public void addViews(ViewsContentType viewsContentType, Long id) {
+        ValueOperations<String, Integer> ops = redisTemplate.opsForValue();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
+        String dateString = dateFormat.format(new Date());
+        String key = resolveKeyForAddingView(viewsContentType, id, dateString);
+        Integer views = ops.get(key);
+
+        if (views == null) {
+            ops.set(key, 1);
+        } else {
+            ops.set(key, views + 1);
+        }
     }
 
-    private String resolveKey(ViewsContentType viewsContentType, Long id, String clientIp) {
+    private String resolveKeyForDuplicateView(ViewsContentType viewsContentType, Long id, String clientIp) {
         return viewsContentType.toString() + id + '_' + clientIp;
+    }
+
+    private String resolveKeyForAddingView(ViewsContentType viewsContentType, Long id, String dateString) {
+        return viewsContentType.toString() + id + '_' + dateString;
     }
 
 }
