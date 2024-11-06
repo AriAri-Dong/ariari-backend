f38ef8ed87f0b853b7883eff313f960f3d762a14
1sunJ <won9619v@naver.com>
2024-11-06 17:39:50 +0900

    Feat: ViewsScheduler 테스트를 위한 로그 추가

--- src/main/java/com/ariari/ariari/commons/manager/views/ViewsScheduler.java
+++ src/main/java/com/ariari/ariari/commons/manager/views/ViewsScheduler.java
@@ -5,12 +5,17 @@ import com.ariari.ariari.domain.club.ClubRepository;
 import com.ariari.ariari.domain.recruitment.Recruitment;
 import com.ariari.ariari.domain.recruitment.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * 테스트를 위한 로그가 포함
+ * 테스트 이후 삭제
+ */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ViewsScheduler {
@@ -21,16 +26,23 @@ public class ViewsScheduler {
 
     @Scheduled(cron = "0 0 4 * * *")
     public void updateViews() {
+        log.info("===== Updating views start =====");
         List<Club> clubs = clubRepository.findAll();
         List<Recruitment> recruitments = recruitmentRepository.findAll();
 
         for (Club club : clubs) {
+            log.info("clubid {} before : {}", club.getId(), club.getViews());
             viewsManager.subtractViews(club);
+            log.info("clubid {} after : {}", club.getId(), club.getViews());
+
         }
 
         for (Recruitment recruitment : recruitments) {
+            log.info("clubid {} before : {}", recruitment.getId(), recruitment.getViews());
             viewsManager.subtractViews(recruitment);
+            log.info("clubid {} after : {}", recruitment.getId(), recruitment.getViews());
         }
+        log.info("===== Updating views end =====");
     }
 
 }
