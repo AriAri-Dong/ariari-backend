21cad2a3e9aa2348c58b20a1f66b148799c1f881
1sunJ <won9619v@naver.com>
2024-11-10 00:48:28 +0900

    Feat : ClubMemberRepository 메서드 추가

--- src/main/java/com/ariari/ariari/domain/clubmember/ClubMemberRepository.java
+++ src/main/java/com/ariari/ariari/domain/clubmember/ClubMemberRepository.java
@@ -1,6 +1,22 @@
 package com.ariari.ariari.domain.clubmember;
 
+import com.ariari.ariari.domain.club.Club;
+import com.ariari.ariari.domain.member.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+
+    Page<ClubMember> findByMember(Member member, Pageable pageable);
+
+    Page<ClubMember> findByClub(Club club, Pageable pageable);
+
+    Optional<ClubMember> findByMemberAndClub(Member member, Club club);
+
+
+
+
 }
