8fad174b2a726793134c7524642dcc7f9b2c630a
haus <won9619v@naver.com>
2024-11-11 22:07:27 +0900

    Feat : Club 삭제 기능 추가

--- src/main/java/com/ariari/ariari/domain/club/ClubService.java
+++ src/main/java/com/ariari/ariari/domain/club/ClubService.java
@@ -2,6 +2,7 @@ package com.ariari.ariari.domain.club;
 
 import com.ariari.ariari.commons.exception.exceptions.NotFoundEntityException;
 import com.ariari.ariari.commons.manager.views.ViewsManager;
+import com.ariari.ariari.domain.club.deletedclub.DeletedClub;
 import com.ariari.ariari.domain.club.deletedclub.DeletedClubRepository;
 import com.ariari.ariari.domain.club.dto.ClubDetailRes;
 import com.ariari.ariari.domain.club.dto.ClubModifyReq;
@@ -12,9 +13,16 @@ import com.ariari.ariari.domain.clubmember.ClubMemberRepository;
 import com.ariari.ariari.domain.clubmember.enums.ClubMemberRoleType;
 import com.ariari.ariari.domain.member.Member;
 import com.ariari.ariari.domain.member.MemberRepository;
+import com.ariari.ariari.domain.recruitment.Recruitment;
+import com.ariari.ariari.domain.recruitment.RecruitmentRepository;
+import com.ariari.ariari.domain.recruitment.deletedrecruitment.DeletedRecruitment;
+import com.ariari.ariari.domain.recruitment.deletedrecruitment.DeletedRecruitmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,9 +33,11 @@ public class ClubService {
     private final ClubRepository clubRepository;
     private final ClubMemberRepository clubMemberRepository;
     private final DeletedClubRepository deletedClubRepository;
+    private final RecruitmentRepository recruitmentRepository;
+    private final DeletedRecruitmentRepository deletedRecruitmentRepository;
     private final ViewsManager viewsManager;
 
-    public ClubDetailRes findClubDetail(Long memberId, Long clubId, String clientIp) {
+    public ClubDetailRes findClubDetail(Long reqMemberId, Long clubId, String clientIp) {
         Club club = clubRepository.findById(clubId).orElseThrow(NotFoundEntityException::new);
 
         // views 처리
@@ -39,25 +49,26 @@ public class ClubService {
         return ClubDetailRes.fromEntity(club);
     }
 
-    public void saveClub(Long memberId, ClubSaveReq saveReq) {
-        Member member = memberRepository.findById(memberId).orElseThrow(NotFoundEntityException::new);
+    public void saveClub(Long reqMemberId, ClubSaveReq saveReq) {
+        Member reqMember = memberRepository.findById(reqMemberId).orElseThrow(NotFoundEntityException::new);
 
         Club club = saveReq.toEntity();
+        club.setHasRecruitment(Boolean.FALSE);
         clubRepository.save(club);
 
         // 요청 Member 를 admin ClubMember 로 저장
         ClubMember clubMember = ClubMember.builder()
                 .club(club)
-                .member(member)
+                .member(reqMember)
                 .clubMemberRoleType(ClubMemberRoleType.ADMIN)
                 .build();
         clubMemberRepository.save(clubMember);
     }
 
-    public void modifyClub(Long memberId, Long clubId, ClubModifyReq modifyReq) {
-        Member member = memberRepository.findById(memberId).orElseThrow(NotFoundEntityException::new);
+    public void modifyClub(Long reqMemberId, Long clubId, ClubModifyReq modifyReq) {
+        Member reqMember = memberRepository.findById(reqMemberId).orElseThrow(NotFoundEntityException::new);
         Club club = clubRepository.findById(clubId).orElseThrow(NotFoundEntityException::new);
-        ClubMember clubMember = clubMemberRepository.findByClubAndMember(club, member).orElseThrow(NotFoundEntityException::new);
+        ClubMember clubMember = clubMemberRepository.findByClubAndMember(club, reqMember).orElseThrow(NotFoundEntityException::new);
 
         // ClubMember 권한 검증
         if (clubMember.getClubMemberRoleType().equals(ClubMemberRoleType.GENERAL)) {
@@ -67,7 +78,32 @@ public class ClubService {
         modifyReq.modifyEntity(club);
     }
 
-    public void removeClub(Long memberId, Long clubId) {
+    /**
+     * 논리 삭제 처리
+     * 연관관계 엔티티 : Recruitment(logical), ...
+     */
+    public void removeClub(Long reqMemberId, Long clubId) {
+        Member reqMember = memberRepository.findById(reqMemberId).orElseThrow(NotFoundEntityException::new);
+        Club club = clubRepository.findById(clubId).orElseThrow(NotFoundEntityException::new);
+        ClubMember clubMember = clubMemberRepository.findByClubAndMember(club, reqMember).orElseThrow(NoClubAuthException::new);
+
+        // ClubMember 권한 검증
+        if (!clubMember.getClubMemberRoleType().equals(ClubMemberRoleType.ADMIN)) {
+            throw new NoClubAuthException();
+        }
+        
+        // 연관관계 처리
+        List<Recruitment> recruitments = club.getRecruitments();
+        for (Recruitment recruitment : recruitments) {
+            DeletedRecruitment deletedRecruitment = DeletedRecruitment.fromRecruitment(recruitment);
+            deletedRecruitmentRepository.save(deletedRecruitment);
+            recruitmentRepository.delete(recruitment);
+        }
+
+        // 논리 삭제 처리
+        DeletedClub deletedClub = DeletedClub.fromClub(club);
+        deletedClubRepository.save(deletedClub);
+        clubRepository.delete(club);
 
     }
 
