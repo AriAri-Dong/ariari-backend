9fd99563295b37e831ad48189c73fbd4a3808cd7
1sunJ <won9619a@gmail.com>
2025-02-11 21:43:32 +0900

    Fix : add ClubMemberRoleTypeModifyReqe

---
+++ src/main/java/com/ariari/ariari/domain/club/clubmember/dto/req/ClubMemberRoleTypeModifyReq.java
@@ -0,0 +1,11 @@
+package com.ariari.ariari.domain.club.clubmember.dto.req;
+
+import com.ariari.ariari.domain.club.clubmember.enums.ClubMemberRoleType;
+import lombok.Data;
+
+@Data
+public class ClubMemberRoleTypeModifyReq {
+
+    private ClubMemberRoleType clubMemberRoleType;
+
+}
