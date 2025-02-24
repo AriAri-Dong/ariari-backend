b339ad16e3da5ed94b1e3ce6ba99b1ba06a4f430
winterholic <iuholic83@naver.com>
2025-02-25 01:12:55 +0900

    스웨거 CORS 수정중

--- src/main/java/com/ariari/ariari/configs/SwaggerConfig.java
+++ src/main/java/com/ariari/ariari/configs/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +30,6 @@ public class SwaggerConfig {
                                 .name("Authorization")
                         )
                 )
-                .addServersItem(new Server().url(TEST_SERVER_URL))
                 .addSecurityItem(new SecurityRequirement().addList("customAuth"));
     }
 
