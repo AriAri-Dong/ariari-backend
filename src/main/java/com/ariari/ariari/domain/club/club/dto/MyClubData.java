290117e158de67b160e5e5061df68f91a0fb04f5
1sunJ <won9619v@naver.com>
2025-04-13 17:07:57 +0900

    Feat : 내 동아리 리스트 조회 기능 추가

--- src/main/java/com/ariari/ariari/domain/club/club/dto/MyClubData.java
+++ src/main/java/com/ariari/ariari/domain/club/club/dto/MyClubData.java
@@ -1,10 +1,15 @@
 package com.ariari.ariari.domain.club.club.dto;
 
+import com.ariari.ariari.domain.club.Club;
+import com.ariari.ariari.domain.club.activity.ClubActivity;
+import com.ariari.ariari.domain.club.notice.ClubNotice;
 import com.ariari.ariari.domain.school.dto.SchoolData;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Schema(description = "내 동아리 데이터")
@@ -18,7 +23,16 @@ public class MyClubData {
     @Schema(description = "동아리 프로필 이미지 URI", example = "")
     private String profileUri;
 
+    private LocalDateTime clubNoticeCreatedDt;
+    private LocalDateTime clubActivityCreatedDt;
+//    private LocalDateTime eventCreatedDt;
 
-
+    public MyClubData(Club club, ClubNotice clubNotice, ClubActivity clubActivity) {
+        this.id = club.getId();
+        this.name = club.getName();
+        this.profileUri = club.getProfileUri();
+        this.clubNoticeCreatedDt = clubNotice == null ? null : clubNotice.getCreatedDateTime();
+        this.clubActivityCreatedDt = clubActivity == null ? null : clubActivity.getCreatedDateTime();
+    }
 
 }
